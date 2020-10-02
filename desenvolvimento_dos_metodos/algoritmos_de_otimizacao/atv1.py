379ef93f213b432fc0164db0a290f820c75b5466
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-02 15:30:34 -0300

    funcao custo completa

--- desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
+++ desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
@@ -56,3 +56,95 @@ def imprimir_agenda(agenda):
 
 agenda = [1,4,3,2,7,3,6,3,2,4,5,3]
 imprimir_agenda(agenda)
+
+'''definindo o custo de tempo em minutos'''
+
+
+def get_minutos(hora):
+    #convertendo valores string
+    x = time.strptime(hora,'%H:%M')
+    #convertendo horas em minutos
+    minutos = x[3] * 60 + x[4]
+    return minutos
+
+'''definindo função de custo'''
+
+def funcao_custo(solucao):
+    preco_total = 0
+    ultima_chegada = 0
+    #voo mais tarde 23:59
+    primeira_partida = 1439
+    
+    id_voo = -1
+    for i in range(len(solucao)//2):
+        #aeroporto de origem
+        origem = pessoas[i][1]
+        id_voo +=1
+        ida = voos[(origem,destino)][solucao[id_voo]]
+        id_voo+=1
+        volta = voos[(destino,origem)][solucao[id_voo]]
+        
+        preco_total += ida[2]
+        preco_total +=volta[2]
+        
+        if ultima_chegada < get_minutos(ida[1]):
+            ultima_chegada = get_minutos(ida[1])
+            
+            
+        if primeira_partida > get_minutos(volta[0]):
+            primeira_partida = get_minutos(volta[0])
+        
+        
+    total_espera = 0 
+    id_voo = -1
+    for i in range(len(solucao)//2):
+        origem = pessoas[i][1]
+        id_voo +=1
+        ida = voos[(origem,destino)][solucao[id_voo]]
+        id_voo+=1
+        volta = voos[(destino,origem)][solucao[id_voo]]
+        
+        total_espera += ultima_chegada - get_minutos(ida[1])
+        total_espera += get_minutos(volta[0]) - primeira_partida
+    if ultima_chegada > primeira_partida:
+        preco_total += 50
+        
+    return preco_total + total_espera
+        
+        
+            
+        
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
