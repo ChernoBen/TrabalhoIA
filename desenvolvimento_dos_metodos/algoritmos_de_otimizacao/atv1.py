b07d5afd3aa050e50602e357949ed9cce47f3c01
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-02 17:38:19 -0300

    resolvido problema funcao impressão

--- desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
+++ desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
@@ -38,24 +38,27 @@ for linha in open('recursos/voos.txt'):
 '''listar voos escolhidos'''
 
 def imprimir_agenda(agenda):
+    contador = 0
     #variavel utilizada para percorrer registros da lista
     id_voo = -1
     # // divide e retorna valor inteiro
     for i in range(len(agenda)//2):
+        contador +=1
         #obter nome da pessoa
         nome = pessoas[i][0]
         #obter aeroporto de partida
         origem = pessoas[i][1]
         id_voo += 1
-        ida = voos[(origem,destino)][agenda[id_voo]]
+        ida = voos[(origem, destino)][agenda[id_voo]]
         id_voo += 1
-        volta = voos[(destino,origem)][agenda[id_voo]]
+        volta = voos[(destino, origem)][agenda[id_voo]]
         print('%10s%10s %5s-%5s  R$%3s %5s-%5s R$%3s' % (nome, origem, ida[0],
               ida[1], ida[2], volta[0], volta[1], volta[2]))
+    print(contador)
         
 
-agenda = [1,4,3,2,7,3,6,3,2,4,5,3]
-imprimir_agenda(agenda)
+agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
+#imprimir_agenda(agenda)
 
 ''' ---Inicio função de custo ---
     definindo o custo de tempo em minutos'''
@@ -76,12 +79,12 @@ def funcao_custo(solucao):
     primeira_partida = 1439
     
     id_voo = -1
-    for i in range(len(solucao)//2):
+    for i in range(len(solucao) // 2):
         #aeroporto de origem
         origem = pessoas[i][1]
         id_voo +=1
         ida = voos[(origem,destino)][solucao[id_voo]]
-        id_voo+=1
+        id_voo +=1
         volta = voos[(destino,origem)][solucao[id_voo]]
         
         preco_total += ida[2]
@@ -97,15 +100,16 @@ def funcao_custo(solucao):
         
     total_espera = 0 
     id_voo = -1
-    for i in range(len(solucao)//2):
+    for i in range(len(solucao) // 2):
         origem = pessoas[i][1]
         id_voo +=1
         ida = voos[(origem,destino)][solucao[id_voo]]
-        id_voo+=1
+        id_voo +=1
         volta = voos[(destino,origem)][solucao[id_voo]]
         
         total_espera += ultima_chegada - get_minutos(ida[1])
         total_espera += get_minutos(volta[0]) - primeira_partida
+        
     if ultima_chegada > primeira_partida:
         preco_total += 50
         
@@ -121,9 +125,9 @@ def perquisa_randomica(dominio,funcao_custo):
         solucao = [random.randint(dominio[i][0],dominio[i][1]) for i in range(len(dominio))]
         custo = funcao_custo(solucao)
         if custo < melhor_custo:
-            melhor_custo = solucao
-            return melhor_custo
-    return solucao
+            melhor_custo = custo
+            melhor_solucao = solucao
+    return melhor_solucao
     
 
 dominio = [(0,9)] * (len(pessoas) * 2) 
