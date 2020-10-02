beeb17284d0f04f43893c55d8b81e0ddb9c24ada
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-02 14:27:21 -0300

    resolvendo problema da formatação da impressão

--- desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
+++ desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
@@ -47,10 +47,11 @@ def imprimir_agenda(agenda):
         #obter aeroporto de partida
         origem = pessoas[i][1]
         id_voo += 1
-        ida = voos[origem, destino][agenda[id_voo]]
+        ida = voos[(origem,destino)][agenda[id_voo]]
         id_voo += 1
         volta = voos[(destino,origem)][agenda[id_voo]]
-        print('%10s%10s %5s-%5s  R$%3s %5s-%5s R$%3s' % nome,origem,ida[0],ida[1],ida[2],volta[0],volta[1,volta[2]])
-        
+        print(nome,origem,ida[0],ida[1],ida[2],volta[0],volta[1],volta[2])
+    
 
-agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
+agenda = [1,4,3,2,7,3,6,3,2,4,5,3]
+imprimir_agenda(agenda)
