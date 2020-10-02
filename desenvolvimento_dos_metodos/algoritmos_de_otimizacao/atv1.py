4de03450b22357e3652bb4e4f96834effb6468fe
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-02 14:17:50 -0300

    definindo função de agenda de voos

--- desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
+++ desenvolvimento_dos_metodos/algoritmos_de_otimizacao/atv1.py
@@ -35,3 +35,22 @@ for linha in open('recursos/voos.txt'):
     voos[(_origem,_destino)].append((_saida,_chegada,int(_preco)))
 
 
+'''listar voos escolhidos'''
+
+def imprimir_agenda(agenda):
+    #variavel utilizada para percorrer registros da lista
+    id_voo = -1
+    # // divide e retorna valor inteiro
+    for i in range(len(agenda)//2):
+        #obter nome da pessoa
+        nome = pessoas[i][0]
+        #obter aeroporto de partida
+        origem = pessoas[i][1]
+        id_voo += 1
+        ida = voos[origem, destino][agenda[id_voo]]
+        id_voo += 1
+        volta = voos[(destino,origem)][agenda[id_voo]]
+        print('%10s%10s %5s-%5s  R$%3s %5s-%5s R$%3s' % nome,origem,ida[0],ida[1],ida[2],volta[0],volta[1,volta[2]])
+        
+
+agenda = [1,4, 3,2, 7,3, 6,3, 2,4, 5,3]
