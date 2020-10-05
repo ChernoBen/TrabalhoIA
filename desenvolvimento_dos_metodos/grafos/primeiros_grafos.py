4f9d5a3a99ce3fca91cf592976a80e8b0306463b
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-05 18:52:33 -0300

    criando dataframe do tabuleiro

--- desenvolvimento_dos_metodos/grafos/primeiros_grafos.py
+++ desenvolvimento_dos_metodos/grafos/primeiros_grafos.py
@@ -4,20 +4,30 @@ Created on Sun Oct  4 19:41:08 2020
 
 @author: Benjamim
 """
+import pandas as pd
+import numpy as np
 import random
+
+
 n = 8
 '''gerando representação do problema'''
-matrix = []
-
+lista = []
+colunas = [i for i in range(n)]
+df = pd.DataFrame( index=(colunas), columns=(colunas))
+i,j=0,0
 for i in range(n):
     for j in range(n):
        print(i,j)
-       matrix.append((i,j))
+       lista.append((i,j))
+       df[i][j]= (i,j)
+       
+
+
        
 '''vetor de estado - sorteio de posições''' 
 vetor_estado = []
 for x in range(8):
-    vetor_estado.append(random.choice(matrix))
+    vetor_estado.append(random.choice(lista))
     
     
 '''funcao objetivo'''
