e301e5738ae9c5c70cab54a7aa9f3a22a520b303
ChernoBen <bneto@computacao.cua.ufmt.br>
2020-10-07 21:21:01 -0300

    problemas com a solução aleatoria de valores acima de 16

--- hill_climbing/hc_desenvolv.py
+++ hill_climbing/hc_desenvolv.py
@@ -6,26 +6,33 @@ Created on Wed Oct  7 10:33:11 2020
 """
 import pandas as pd
 import numpy as np
-from livelossplot import PlotLosses
+#from livelossplot import PlotLosses
 import random
-from deap import creator, base, tools, algorithms
+#from deap import creator, base, tools, algorithms
 
 
 #N = Número de rainhas
-N = 8
+N = 24
 #log_N = número de bits para representar cada rainha
 log_N = int(np.log2(N))
-
+#criando tabuleiro
+colunas = [i for i in range(N)]
+tabuleiro = pd.DataFrame(index = (colunas),columns = (colunas))
 
 #Funções extraídas do código original do livro
 #https://github.com/aimacode/aima-python
 def conflicted(state, row, col):
     """Would placing a queen at (row, col) conflict with anything?"""
+    #print(state,row,col) 
+    #print(any(conflict(row, col, state[h],h)
+               #for h in range(col)))
     return any(conflict(row, col, state[c], c)
                for c in range(col))
 
 def conflict(row1, col1, row2, col2):
     """Would putting two queens in (row1, col1) and (row2, col2) conflict?"""
+    #rint(row1,col1,row2,col2)
+    
     return (row1 == row2 or  # same row
             col1 == col2 or  # same column
             row1 - col1 == row2 - col2 or  # same \ diagonal
@@ -41,12 +48,15 @@ def goal_test(state):
 def h(node):
     """Return number of conflicting queens for a given node"""
     num_conflicts = 0
+    confli_list =[]
     for (r1, c1) in enumerate(node):
         #print('r1 e c1',(r1,c1))
         for (r2, c2) in enumerate(node):
             #print((r1,c1),':',(r2,c2))
             if (r1, c1) != (r2, c2):
                 num_conflicts += conflict(r1, c1, r2, c2)
+                confli_list.append([r1, c1, r2, c2])
+    #print(confli_list)    
     
     #print("numero conf",num_conflicts)
     return num_conflicts
@@ -59,16 +69,14 @@ def nqueen_fitness(node):
 
     return h(node_int),
 
-goal_test([2,4,6,8,3,1,7,4])
-h([2,2,6,7,3,1,7,4])
-
-
+#goal_test([2,4,6,8,3,1,7,4])
+hh = h([5,2,0,7,4,1,3,6])
 
 
 '''codigo do professor até aqui'''
 arr = [i for i in range(N)]
 random.shuffle(arr)
-param = [3,5,0,7,6,4,2,1]
+param = arr
 
 
 def hill_clim(arr):
@@ -77,29 +85,30 @@ def hill_clim(arr):
     contador = 1
     selects = arr[1:len(arr)]
     while True:
-        contador = 1
-        for i in range(len(selects)):
-            arr2[contador] = selects[i]
-            contador +=1
+        
         if goal_test(arr2) != False:
             #result = [j for j in arr2]
             #for x in range(len(arr1)):
                 #print(x,arr2[x])
             return arr2
+        
+        contador = 1
+        for i in range(len(selects)):
+            arr2[contador] = selects[i]
+            contador +=1
+        
         else:
             print(arr2)
             random.shuffle(selects)
 
 
+
 eiy = [y for y in range(N)]
 antes = goal_test(param)
-eix = hill_clim([3,5,0,7,6,4,2,1])
+eix = hill_clim(param)
 depois = goal_test(param)
 
-'''criando tabuleiro'''
-colunas = [i for i in range(N)]
 
-tabuleiro = pd.DataFrame(index = (colunas),columns = (colunas))
 
 '''adicionando rainhas em suas posições'''
 for i in range(len(eiy)):
@@ -110,5 +119,4 @@ for i in range(len(eiy)):
 
 
 
-
-
+[5,2,0,7,4,1,3,6]
